c7f63fa80f502f4c978f2997cd8c52a6ffaec6d1
Natnael-Negash <nnegash658@gmail.com>
2024-08-13 04:09:49 -0900

    data manuplation

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+function getListStudents(){
+    return[
+        {id: 1, firstname: 'Guillaume', location:'San Francisco'},
+        {id: 2, firstname: 'james', location: 'columbia'},
+        {id: 5, firstname: 'Serena', location: 'SanFrancisco'}
+    ];
+}
